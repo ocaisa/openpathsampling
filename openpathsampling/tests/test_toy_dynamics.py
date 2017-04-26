5f8413816a0f48c4dfb65ad71e094b4cfd96e4a2
Andreas Singraber <andreas.singraber@gmx.at>
2017-04-26 15:04:48 +0200

    Added Toy PES: double well potential.
    - Docstrings and Tests done.

--- openpathsampling/tests/test_toy_dynamics.py
+++ openpathsampling/tests/test_toy_dynamics.py
@@ -23,11 +23,12 @@ import numpy as np
 
 def setUp():
     # set up globals
-    global gaussian, linear, outer, harmonic
+    global gaussian, linear, outer, harmonic, doublewell
     gaussian = toy.Gaussian(6.0, [2.5, 40.0], [0.8, 0.5])
     outer = toy.OuterWalls([1.12, 2.0], [0.2, -0.25])
     linear = toy.LinearSlope([1.5, 0.75], 0.5)
     harmonic = toy.HarmonicOscillator([1.5, 2.0], [0.5, 3.0], [0.25, 0.75])
+    doublewell = toy.DoubleWell([5.0, 2.0], [2.0, 3.0])
     global init_pos, init_vel, sys_mass
     init_pos = np.array([0.7, 0.65])
     init_vel = np.array([0.6, 0.5])
@@ -99,6 +100,20 @@ class testLinearSlope(object):
 
     def test_dVdx(self):
         assert_equal(linear.dVdx(self), [1.5, 0.75])
+
+class testDoubleWell(object):
+    def setUp(self):
+        self.positions = init_pos
+        self.velocities = init_vel
+        self.mass = sys_mass
+
+    def test_V(self):
+        assert_almost_equal(doublewell.V(self), 208.7475125)
+
+    def test_dVdx(self):
+        for (experiment, theory) in zip(doublewell.dVdx(self),
+                                        [-49.14, -44.603]):
+            assert_almost_equal(experiment, theory)
 
 class testCombinations(object):
     def setUp(self):
